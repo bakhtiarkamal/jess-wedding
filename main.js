bfbb795ed04e5917a5cd2a0d791ad9cd7ca8d282
Bakhtiar Kamal <bakhtiar.kamal@rsllifecare.org.au>
2019-09-16 12:03:51 +1000

    Updated 404 references

--- main.js
+++ main.js
@@ -2,94 +2,92 @@ var menuStat = "closed";
 var optionNum = $(".nav-option").length;
 
 function init_map() {
-  var myOptions = {
-    scrollwheel: false,
-    zoom: 16,
-    mapTypeControl: false,
-    center: new google.maps.LatLng(-32.22278, 152.530364),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+    var myOptions = {
+        scrollwheel: false,
+        zoom: 16,
+        mapTypeControl: false,
+        center: new google.maps.LatLng(-32.22278, 152.530364),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
-  map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);
+    map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);
 
-  map.set("styles", [
-    {
-      featureType: "road",
-      elementType: "geometry",
-      stylers: [{ color: "#949896" }, { weight: 0.25 }, { lightness: 75 }]
-    },
-    {
-      featureType: "landscape",
-      elementType: "geometry",
-      stylers: [
-        { hue: "#f9f7e8" },
-        { gamma: 1.4 },
-        { saturation: 82 },
-        { lightness: 96 }
-      ]
-    },
-    {
-      featureType: "water",
-      stylers: [{ color: "#8cbfc9" }, { lightness: 50 }]
-    },
-    {
-      featureType: "poi",
-      stylers: [{ visibility: "simplified" }]
-    },
-    {
-      featureType: "poi.park",
-      stylers: [{ color: "#89cb97" }]
-    }
-  ]);
+    map.set("styles", [{
+            featureType: "road",
+            elementType: "geometry",
+            stylers: [{ color: "#949896" }, { weight: 0.25 }, { lightness: 75 }]
+        },
+        {
+            featureType: "landscape",
+            elementType: "geometry",
+            stylers: [
+                { hue: "#f9f7e8" },
+                { gamma: 1.4 },
+                { saturation: 82 },
+                { lightness: 96 }
+            ]
+        },
+        {
+            featureType: "water",
+            stylers: [{ color: "#8cbfc9" }, { lightness: 50 }]
+        },
+        {
+            featureType: "poi",
+            stylers: [{ visibility: "simplified" }]
+        },
+        {
+            featureType: "poi.park",
+            stylers: [{ color: "#89cb97" }]
+        }
+    ]);
 
-  var icon = {
-    url: "images/liberty-map.png", // url
-    scaledSize: new google.maps.Size(80, 80) // scaled size
-  };
+    var icon = {
+        url: "images/map.jpg", // url
+        scaledSize: new google.maps.Size(80, 80) // scaled size
+    };
 
-  marker = new google.maps.Marker({
-    icon: icon,
-    map: map,
-    position: new google.maps.LatLng(-32.22278, 152.530364)
-  });
-  infowindow = new google.maps.InfoWindow({
-    content:
-      "<strong>The Gardens at Lakeside</strong><br> 13 Tea Tree Rd, Forster NSW 2428<br>"
-  });
-  google.maps.event.addListener(marker, "click", function() {
+    marker = new google.maps.Marker({
+        icon: icon,
+        map: map,
+        position: new google.maps.LatLng(-32.22278, 152.530364)
+    });
+    infowindow = new google.maps.InfoWindow({
+        content: "<strong>The Gardens at Lakeside</strong><br> 13 Tea Tree Rd, Forster NSW 2428<br>"
+    });
+    google.maps.event.addListener(marker, "click", function() {
+        infowindow.open(map, marker);
+    });
     infowindow.open(map, marker);
-  });
-  infowindow.open(map, marker);
 }
 
 $(document).ready(function() {
-  google.maps.event.addDomListener(window, "load", init_map);
-  var tempWidth;
-  tempWidth = $("#mobileBG").width();
-  $("#mobileBG").css("left", -(tempWidth / 2) + $(document).width() / 3);
+    google.maps.event.addDomListener(window, "load", init_map);
+    var tempWidth;
+    tempWidth = $("#mobileBG").width();
+    $("#mobileBG").css("left", -(tempWidth / 2) + $(document).width() / 3);
 });
 
 $(window).load(function() {
-  new WOW().init();
-  parallax();
-  $(".loader").css("display", "none");
-  $("#siteContainer").css("opacity", "1");
-  $("#txtCon").addClass("animatedLong fadeInUp");
-  $("#hashtag").addClass("animated fadeIn");
-  $("#Monogram").addClass("animated fadeIn");
-  $("#page1BG").addClass("animatedLong fadeIn");
+    new WOW().init();
+    parallax();
+    $(".loader").css("display", "none");
+    $("#siteContainer").css("opacity", "1");
+    $("#txtCon").addClass("animatedLong fadeInUp");
+    //$("#hashtag").addClass("animated fadeIn");
+    //$("#Monogram").addClass("animated fadeIn");
+    $("#page1BG").addClass("animatedLong fadeIn");
 });
 
 $(window).scroll(function(e) {
-  parallax();
+    parallax();
 });
 
 function parallax() {
-  var scrolled = $(window).scrollTop();
-  var page2 = $("#Page2").offset().top;
-  $("#D1").css({ transform: "translate3d(0," + scrolled * 0.2 + "px,0)" });
-  $("#D2").css({ transform: "translate3d(0," + scrolled * 0.5 + "px,0)" });
-  $("#D3").css({ transform: "translate3d(0," + scrolled * 0.2 + "px,0)" });
-  $("#D4").css({ transform: "translate3d(0," + scrolled * 0.2 + "px,0)" });
-  $("#D5").css({ transform: "translate3d(0," + scrolled * 0.3 + "px,0)" });
+    var scrolled = $(window).scrollTop();
+    var page2 = $("#Page2").offset().top;
+    $("#D1").css({ transform: "translate3d(0," + scrolled * 0.2 + "px,0)" });
+    $("#D2").css({ transform: "translate3d(0," + scrolled * 0.5 + "px,0)" });
+    $("#D3").css({ transform: "translate3d(0," + scrolled * 0.2 + "px,0)" });
+    $("#D4").css({ transform: "translate3d(0," + scrolled * 0.2 + "px,0)" });
+    $("#D5").css({ transform: "translate3d(0," + scrolled * 0.3 + "px,0)" });
 }
